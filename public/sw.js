cfab50c125c00d688cdf3f37d5e4ea94ed8a7af0
Musheer <musheermevon@gmail.com>
2026-07-01 21:28:15 +0530

    Monetag code removed

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,3 @@
-self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 11225448
-}
-self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
-
 const CACHE_NAME = 'dtl-v1'
 const STATIC_ASSETS = [
   '/',
